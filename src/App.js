7d0e1238ffbdec3e95b43e3a07389f789bb22b92
CTS\588960 <vinay.m2@cognizant.com>
2019-02-27 13:17:02 +0530

    my-second-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 const isSearched = (searchTerm) => (item) =>
   !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -25,39 +12,66 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
+
+    this.setSearchTopstories = this.setSearchTopstories.bind(this);
+    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onSearchChange = this.onSearChange.bind(this);
   }
+
+  setSearchTopstories(result) {
+    this.setState({ result });
+  }
+  fetchSearchTopstories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopstories(result));
+  }
   onDismiss(id) {
-    function isNotId(item) {
-      return item.objectID !== id;
-    }
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      //result: Object.assign({}, this.state.result, { hits: updatedHits })
+      result: { ...this.state.result, hits: updatedHits }
+    });
+  }
+
+  onSearchSubmit() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopstories(searchTerm);
   }
 
   onSearChange(e) {
     this.setState({ searchTerm: e.target.value });
   }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopstories(searchTerm);
+  }
+
   render() {
     const helloWorld = 'Welcome to React!';
     const user = {
       firstName: 'Vinay',
       lastName: 'M'
     }
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    if (!result) { return null; }
     return (
       <div className="App">
         <div className="page">
           <h2>{helloWorld}</h2>
           <span>{user.firstName}{user.lastName}</span>
           <div className="interactions">
-            <Search value={searchTerm} onChange={this.onSearchChange}>Search:</Search>
+            <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>Search:</Search>
           </div>
-          <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+          {result ? <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
+            : null}
         </div>
       </div>
     );
@@ -65,14 +79,21 @@ class App extends Component {
 }
 class Search extends Component {
   render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}<input
+    const Search = ({
+      value,
+      onChange,
+      onSubmit,
+      children
+    }) =>
+      <form onSubmit={onSubmit}>
+        <input
           type="text"
           value={value}
           onChange={onChange}
         />
+        <button type="submit">
+          {children}
+        </button>
       </form>
     );
   }
@@ -99,10 +120,9 @@ class Table extends Component {
             <span style={{ width: '10%' }}>
               <Button
                 onClick={() => onDismiss(item.objectID)}
-                className="button-inline"
-              >
+                className="button-inline">
                 Dismiss
-</Button>
+              </Button>
             </span>
           </div>
         )}
